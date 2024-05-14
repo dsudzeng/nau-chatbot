10468ce50209827c78d52ca4e85222b5977e1e58
dsudzeng <38187550+dsudzeng@users.noreply.github.com>
2024-05-13 22:24:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,13 +11,11 @@ st.header('Department of Global Languages and Cultures', divider='rainbow')
 new_title = '<p style="font-family:sans-serif; color:Green; font-size: 30px;">Welcome to the Chinese Program!</p>'
 st.markdown(new_title, unsafe_allow_html=True)
 
-
-st.title('🦜🔗 Quickstart App')
+st.title('🦜🔗 Chatbot')
 
 def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
   st.info(llm(input_text))
 
-with st.form('my_form'):
-  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
-  generate_response(text)
+text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+generate_response(text)
